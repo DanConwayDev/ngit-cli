3bf83e6437a29de9b6fc57a3641d923e1b2a8cb5
DanConwayDev <DanConwayDev@protonmail.com>
2024-02-16 23:02:37 +0000

    test: fix wording in cli test
    broken in 701668b02d999af42f51d8bd25fffb2a8692c3c8

--- tests/pull.rs
+++ tests/pull.rs
@@ -183,9 +183,9 @@ mod when_branch_doesnt_exist {
                 test_repo.checkout("random-name")?;
 
                 let mut p = CliTester::new_from_dir(&test_repo.dir, ["pull"]);
-                p.expect("finding proposal event...\r\n")?;
+                p.expect("finding proposal root event...\r\n")?;
                 p.expect(
-                    "Error: cannot find a proposal event associated with the checked out branch name\r\n",
+                    "Error: cannot find a proposal root event associated with the checked out branch name\r\n",
                 )?;
 
                 p.expect_end()?;
