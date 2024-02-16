454c955d60e6f313bf8da6822337cace9bb6fc16
DanConwayDev <DanConwayDev@protonmail.com>
2024-02-16 20:43:14 +0000

    refactor: improve error when patch ahead of master
    minor improvement to error message

--- src/git.rs
+++ src/git.rs
@@ -372,7 +372,7 @@ impl RepoActions for Repo {
             if let Ok(last_patch) = patches_to_apply.last().context("no patches") {
                 last_patch
             } else {
-                self.checkout(branch_name).context("latest commit in pr doesnt connect with an existing commit. Try a git pull first.")?;
+                self.checkout(branch_name).context("latest commit in pr doesnt connect with an existing commit. Try a git pull on master first.")?;
                 return Ok(vec![]);
             },
             "parent-commit",
