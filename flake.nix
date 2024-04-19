4b49ab05e3b8fa3146d7dfd0c8e5250c14df46ae
DanConwayDev <DanConwayDev@protonmail.com>
2024-04-19 16:13:21 +0100

    build: fix lint warning
    remove unused binding

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
