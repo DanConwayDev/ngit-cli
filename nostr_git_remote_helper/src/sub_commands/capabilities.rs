9ba3dc08e60906cef6c82a375d3c427d2b2f4733
DanConwayDev <DanConwayDev@protonmail.com>
2024-07-02 07:46:28 +0100

    refactor: remove old remote helper placeholder
    to make room for new one

--- nostr_git_remote_helper/src/sub_commands/capabilities.rs
+++
@@ -1,8 +0,0 @@
-use anyhow::Result;
-
-// https://git-scm.com/docs/gitremote-helpers#_capabilities
-pub async fn launch() -> Result<()> {
-    // blank line indicates end of capabilities
-    println("");
-    Ok(())
-}
