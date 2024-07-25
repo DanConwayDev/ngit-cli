adc9b8d43d208178269d0f1178255d933ae1c4a5
DanConwayDev <DanConwayDev@protonmail.com>
2024-07-25 07:56:05 +0100

    test: reintroduce `match_event` to test queries
    removed in fc3f22eac2bb81823f170f61ba9d39baff76b933 as rust-nostr
    made it private
    
    attempts were made to reintroduce it in
    https://github.com/DanConwayDev/ngit-cli/pull/5
    
    finally we can close #5, the last github PR

--- test_utils/src/relay.rs
+++ test_utils/src/relay.rs
@@ -97,23 +97,15 @@ impl<'a> Relay<'a> {
         client_id: u64,
         subscription_id: &nostr::SubscriptionId,
         // TODO: enable filters
-        _filters: &[nostr::Filter],
+        filters: &[nostr::Filter],
     ) -> Result<bool> {
-        // let t: Vec<nostr::Kind> = self.events.iter().map(|e| e.kind).collect();
-        // .filter(|e| filters.iter().any(|filter| filter.match_event(e)))
-        // println!("letsgo{:?}", t);
-
         self.respond_events(
             client_id,
             subscription_id,
             &self
                 .events
                 .iter()
-                // FIXME:
-                // `filter.match_events` does not exist anymore
-                // it has been moved to `nostr_database_::FilterIndex`
-                // but it's private now
-                // .filter(|e| filters.iter().any(|filter|filter.match_event(e)))
+                .filter(|e| filters.iter().any(|filter| filter.match_event(e)))
                 .filter(|_| true)
                 .cloned()
                 .collect(),
