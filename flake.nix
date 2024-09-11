0ec32cf5e63ad2b97ff8815a65703355f7db92ee
DanConwayDev <DanConwayDev@protonmail.com>
2024-09-11 12:25:47 +0100

    build: remove PKG_CONFIG_PATH env var
    as this is not needed. it was originally added as part of diagnosing
    openssl availablity

--- flake.nix
+++ flake.nix
@@ -63,9 +63,6 @@
               pkg-config # required by git2
               openssl
             ];
-            shellHook = ''
-              export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig"
-            '';
           };
       }
     );
