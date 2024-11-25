DanConwayDev/ngit-cli

commit f79014235e85554e3661b3f2a02b8fa88bc192ff

feat(login): overhaul login experience
* simplify login menu, making it more accessable to newcomers and
  easier to select remote signer options
* enable `ngit login` to work from anywhere (not just a git repo)
* assume fresh login details saved to global git config but fallback
  to local repository
* maintain local repository login via `ngit login --local`
* maintain login via CLI arguments eg `ngit send --nsec nsec123`
* nudge users to remember nsec when pasting in ncryptsec for a
  better UX, whilst maintaining the option to be prompted for
  password everytime
* create placeholder menu items for help menu and create account

src/lib/cli_interactor.rs
@@ -24,11 +24,13 @@ impl InteractorPrompt for Interactor {
         if !parms.default.is_empty() {
             input.default(parms.default);
         }
+        input.report(parms.report);
         Ok(input.interact_text()?)
     }
     fn password(&self, parms: PromptPasswordParms) -> Result<String> {
         let mut p = Password::with_theme(&self.theme);
         p.with_prompt(parms.prompt);
+        p.report(parms.report);
         if parms.confirm {
             p.with_confirmation("confirm password", "passwords didnt match...");
         }
@@ -44,10 +46,10 @@ impl InteractorPrompt for Interactor {
     }
     fn choice(&self, parms: PromptChoiceParms) -> Result<usize> {
         let mut choice = dialoguer::Select::with_theme(&self.theme);
-        choice
-            .with_prompt(parms.prompt)
-            .report(parms.report)
-            .items(&parms.choices);
+        if !parms.prompt.is_empty() {
+            choice.with_prompt(parms.prompt);
+        }
+        choice.report(parms.report).items(&parms.choices);
         if let Some(default) = parms.default {
             if std::env::var("NGITTEST").is_err() {
                 choice.default(default);
@@ -73,6 +75,7 @@ impl InteractorPrompt for Interactor {
 pub struct PromptInputParms {
     pub prompt: String,
     pub default: String,
+    pub report: bool,
     pub optional: bool,
 }
 
@@ -89,12 +92,18 @@ impl PromptInputParms {
         self.optional = true;
         self
     }
+
+    pub fn dont_report(mut self) -> Self {
+        self.report = false;
+        self
+    }
 }
 
 #[derive(Default)]
 pub struct PromptPasswordParms {
     pub prompt: String,
     pub confirm: bool,
+    pub report: bool,
 }
 
 impl PromptPasswordParms {
@@ -104,6 +113,10 @@ impl PromptPasswordParms {
     }
     pub const fn with_confirm(mut self) -> Self {
         self.confirm = true;
+        self
+    }
+    pub fn dont_report(mut self) -> Self {
+        self.report = false;
         self
     }
 }
@@ -140,10 +153,11 @@ impl PromptChoiceParms {
         self
     }
 
-    // pub fn dont_report(mut self) -> Self {
-    //     self.report = false;
-    //     self
-    // }
+    pub fn dont_report(mut self) -> Self {
+        self.report = false;
+        self
+    }
+
     pub fn with_choices(mut self, choices: Vec<String>) -> Self {
         self.choices = choices;
         self

src/bin/git_remote_nostr/list.rs
@@ -35,7 +35,7 @@ pub async fn run_list(
     for_push: bool,
 ) -> Result<HashMap<String, HashMap<String, String>>> {
     let nostr_state =
-        if let Ok(nostr_state) = get_state_from_cache(git_repo.get_path()?, repo_ref).await {
+        if let Ok(nostr_state) = get_state_from_cache(Some(git_repo.get_path()?), repo_ref).await {
             Some(nostr_state)
         } else {
             None

src/bin/git_remote_nostr/main.rs
@@ -15,7 +15,7 @@ use std::{
 use anyhow::{bail, Context, Result};
 use client::{consolidate_fetch_reports, get_repo_ref_from_cache, Connect};
 use git::{nostr_url::NostrUrlDecoded, RepoActions};
-use ngit::{client, git, login};
+use ngit::{client, git, login::existing::load_existing_login};
 use nostr::nips::nip01::Coordinate;
 use utils::read_line;
 
@@ -36,38 +36,17 @@ async fn main() -> Result<()> {
 
     let mut client = Client::default();
 
-    if git_repo
-        .get_git_config_item("nostr.npub", None)
-        .is_ok_and(|x| x.is_some())
-        && (git_repo
-            .get_git_config_item("nostr.nsec", None)
-            .is_ok_and(|x| x.is_some())
-            || (git_repo
-                .get_git_config_item("nostr.bunker-uri", None)
-                .is_ok_and(|x| x.is_some())
-                && git_repo
-                    .get_git_config_item("nostr.bunker-app-key", None)
-                    .is_ok_and(|x| x.is_some())))
+    if let Ok((signer, _, _)) =
+        load_existing_login(&Some(&git_repo), &None, &None, &None, None, true, false).await
     {
-        if let Ok((signer, _)) = login::launch(
-            &git_repo,
-            &None,
-            &None,
-            &None,
-            &None,
-            Some(&client),
-            false,
-            true,
-        )
-        .await
-        {
-            client.set_signer(signer).await;
-        };
+        // signer for to respond to relay auth request
+        client.set_signer(signer).await;
     }
 
     fetching_with_report_for_helper(git_repo_path, &client, &decoded_nostr_url.coordinates).await?;
 
-    let repo_ref = get_repo_ref_from_cache(git_repo_path, &decoded_nostr_url.coordinates).await?;
+    let repo_ref =
+        get_repo_ref_from_cache(Some(git_repo_path), &decoded_nostr_url.coordinates).await?;
 
     let stdin = io::stdin();
     let mut line = String::new();
@@ -170,7 +149,7 @@ async fn fetching_with_report_for_helper(
     let term = console::Term::stderr();
     term.write_line("nostr: fetching...")?;
     let (relay_reports, progress_reporter) = client
-        .fetch_all(git_repo_path, repo_coordinates, &HashSet::new())
+        .fetch_all(Some(git_repo_path), repo_coordinates, &HashSet::new())
         .await?;
     if !relay_reports.iter().any(std::result::Result::is_err) {
         let _ = progress_reporter.clear();

src/bin/git_remote_nostr/push.rs
@@ -8,7 +8,9 @@ use std::{
 
 use anyhow::{anyhow, bail, Context, Result};
 use auth_git2::GitAuthenticator;
-use client::{get_events_from_cache, get_state_from_cache, send_events, sign_event, STATE_KIND};
+use client::{
+    get_events_from_local_cache, get_state_from_cache, send_events, sign_event, STATE_KIND,
+};
 use console::Term;
 use git::{sha1_to_oid, RepoActions};
 use git2::{Oid, Repository};
@@ -76,7 +78,7 @@ pub async fn run_push(
         _ => list_from_remotes(&term, git_repo, &repo_ref.git_server, decoded_nostr_url),
     };
 
-    let nostr_state = get_state_from_cache(git_repo.get_path()?, repo_ref).await;
+    let nostr_state = get_state_from_cache(Some(git_repo.get_path()?), repo_ref).await;
 
     let existing_state = {
         // if no state events - create from first git server listed
@@ -122,17 +124,8 @@ pub async fn run_push(
         return Ok(());
     }
 
-    let (signer, user_ref) = login::launch(
-        git_repo,
-        &None,
-        &None,
-        &None,
-        &None,
-        Some(client),
-        false,
-        false,
-    )
-    .await?;
+    let (signer, user_ref, _) =
+        login::login_or_signup(&Some(git_repo), &None, &None, Some(client)).await?;
 
     if !repo_ref.maintainers.contains(&user_ref.public_key) {
         for refspec in &git_server_refspecs {
@@ -306,7 +299,7 @@ pub async fn run_push(
         term.write_line("broadcast to nostr relays:")?;
         send_events(
             client,
-            git_repo.get_path()?,
+            Some(git_repo.get_path()?),
             events,
             user_ref.relays.write(),
             repo_ref.relays.clone(),
@@ -897,7 +890,7 @@ async fn get_merged_status_events(
                     // merge commit
                     for parent in commit.parents() {
                         // lookup parent id
-                        let commit_events = get_events_from_cache(
+                        let commit_events = get_events_from_local_cache(
                             git_repo.get_path()?,
                             vec![
                                 nostr::Filter::default()
@@ -1045,7 +1038,7 @@ async fn get_proposal_and_revision_root_from_patch(
                 .clone(),
         )?;
 
-        get_events_from_cache(
+        get_events_from_local_cache(
             git_repo.get_path()?,
             vec![nostr::Filter::default().id(proposal_or_revision_id)],
         )

src/bin/git_remote_nostr/utils.rs
@@ -10,7 +10,7 @@ use anyhow::{bail, Context, Result};
 use git2::Repository;
 use ngit::{
     client::{
-        get_all_proposal_patch_events_from_cache, get_events_from_cache,
+        get_all_proposal_patch_events_from_cache, get_events_from_local_cache,
         get_proposals_and_revisions_from_cache,
     },
     git::{
@@ -108,7 +108,7 @@ pub async fn get_open_proposals(
             .collect();
 
     let statuses: Vec<nostr::Event> = {
-        let mut statuses = get_events_from_cache(
+        let mut statuses = get_events_from_local_cache(
             git_repo_path,
             vec![
                 nostr::Filter::default()

src/bin/ngit/cli.rs
@@ -1,4 +1,6 @@
+use anyhow::{bail, Result};
 use clap::{Parser, Subcommand};
+use ngit::login::SignerInfo;
 
 use crate::sub_commands;
 
@@ -23,6 +25,30 @@ pub struct Cli {
     /// disable spinner animations
     #[arg(long, action)]
     pub disable_cli_spinners: bool,
+}
+
+pub fn extract_signer_cli_arguments(args: &Cli) -> Result<Option<SignerInfo>> {
+    if let Some(nsec) = &args.nsec {
+        Ok(Some(SignerInfo::Nsec {
+            nsec: nsec.to_string(),
+            password: None,
+            npub: None,
+        }))
+    } else if let Some(bunker_uri) = args.bunker_uri.clone() {
+        if let Some(bunker_app_key) = args.bunker_app_key.clone() {
+            Ok(Some(SignerInfo::Bunker {
+                bunker_uri,
+                bunker_app_key,
+                npub: None,
+            }))
+        } else {
+            bail!("cli argument bunker-app-key must be supplied when bunker-uri is")
+        }
+    } else if args.bunker_app_key.is_some() {
+        bail!("cli argument bunker-uri must be supplied when bunker-app-key is")
+    } else {
+        Ok(None)
+    }
 }
 
 #[derive(Subcommand)]

src/bin/ngit/sub_commands/init.rs
@@ -6,7 +6,7 @@ use nostr::{nips::nip01::Coordinate, FromBech32, PublicKey, ToBech32};
 use nostr_sdk::Kind;
 
 use crate::{
-    cli::Cli,
+    cli::{extract_signer_cli_arguments, Cli},
     cli_interactor::{Interactor, InteractorPrompt, PromptInputParms},
     client::{fetching_with_report, get_repo_ref_from_cache, send_events, Client, Connect},
     git::{nostr_url::convert_clone_url_to_https, Repo, RepoActions},
@@ -69,7 +69,8 @@ pub async fn launch(cli_args: &Cli, args: &SubCommandArgs) -> Result<()> {
 
     let repo_ref = if let Some(repo_coordinates) = repo_coordinates.clone() {
         fetching_with_report(git_repo_path, &client, &repo_coordinates).await?;
-        if let Ok(repo_ref) = get_repo_ref_from_cache(git_repo_path, &repo_coordinates).await {
+        if let Ok(repo_ref) = get_repo_ref_from_cache(Some(git_repo_path), &repo_coordinates).await
+        {
             Some(repo_ref)
         } else {
             None
@@ -78,15 +79,11 @@ pub async fn launch(cli_args: &Cli, args: &SubCommandArgs) -> Result<()> {
         None
     };
 
-    let (signer, user_ref) = login::launch(
-        &git_repo,
-        &cli_args.bunker_uri,
-        &cli_args.bunker_app_key,
-        &cli_args.nsec,
+    let (signer, user_ref, _) = login::login_or_signup(
+        &Some(&git_repo),
+        &extract_signer_cli_arguments(cli_args).unwrap_or(None),
         &cli_args.password,
         Some(&client),
-        false,
-        false,
     )
     .await?;
 
@@ -381,7 +378,7 @@ pub async fn launch(cli_args: &Cli, args: &SubCommandArgs) -> Result<()> {
 
     send_events(
         &client,
-        git_repo_path,
+        Some(git_repo_path),
         vec![repo_event],
         user_ref.relays.write(),
         relays.clone(),

src/bin/ngit/sub_commands/list.rs
@@ -12,7 +12,7 @@ use nostr_sdk::Kind;
 use crate::{
     cli_interactor::{Interactor, InteractorPrompt, PromptChoiceParms, PromptConfirmParms},
     client::{
-        fetching_with_report, get_events_from_cache, get_repo_ref_from_cache, Client, Connect,
+        fetching_with_report, get_events_from_local_cache, get_repo_ref_from_cache, Client, Connect,
     },
     git::{str_to_sha1, Repo, RepoActions},
     git_events::{
@@ -37,7 +37,7 @@ pub async fn launch() -> Result<()> {
 
     fetching_with_report(git_repo_path, &client, &repo_coordinates).await?;
 
-    let repo_ref = get_repo_ref_from_cache(git_repo_path, &repo_coordinates).await?;
+    let repo_ref = get_repo_ref_from_cache(Some(git_repo_path), &repo_coordinates).await?;
 
     let proposals_and_revisions: Vec<nostr::Event> =
         get_proposals_and_revisions_from_cache(git_repo_path, repo_ref.coordinates()).await?;
@@ -47,7 +47,7 @@ pub async fn launch() -> Result<()> {
     }
 
     let statuses: Vec<nostr::Event> = {
-        let mut statuses = get_events_from_cache(
+        let mut statuses = get_events_from_local_cache(
             git_repo_path,
             vec![
                 nostr::Filter::default()

commit 82d777ebb3e62dbffc4432ce2d159840fd35df72

test: fix cli interactor without prompt
reverts a change in f79014235e85554e3661b3f2a02b8fa88bc192ff which
caused a subtle change to how choices were displayed that caused
the cli tester error when choices without prompts were displayed eg.
in `ngit list` tests

src/lib/cli_interactor.rs
@@ -46,10 +46,10 @@ impl InteractorPrompt for Interactor {
     }
     fn choice(&self, parms: PromptChoiceParms) -> Result<usize> {
         let mut choice = dialoguer::Select::with_theme(&self.theme);
-        if !parms.prompt.is_empty() {
-            choice.with_prompt(parms.prompt);
-        }
-        choice.report(parms.report).items(&parms.choices);
+        choice
+            .with_prompt(parms.prompt)
+            .report(parms.report)
+            .items(&parms.choices);
         if let Some(default) = parms.default {
             if std::env::var("NGITTEST").is_err() {
                 choice.default(default);