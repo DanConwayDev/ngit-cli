7edbfbc1e04c730ca05cdddd2fdc668a7b17c022
DanConwayDev <DanConwayDev@protonmail.com>
2026-05-22 09:10:43 +0100

    chore: remove packages from legacy test harness from flake.nix
    we had pinned the dependencies to commits in our log file.

--- flake.nix
+++ flake.nix
@@ -69,15 +69,6 @@
           pname = manifest.package.name;
           version = manifest.package.version;
           src = ./.;
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-            outputHashes = {
-              "rexpect-0.5.0" =
-                "0amxyp81r90gfqlx5dnfjsmd403kf5hcw0crzpcmsbaviavxff4y";
-              "simple-websockets-0.1.6" =
-                "0910bbl7p3by18w3wks8gbgdg8879hn2c39z1bkr5pcvfkcxmaf3";
-            };
-          };
           buildInputs = [
             pkg-config # required by git2
             openssl
